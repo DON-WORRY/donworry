4063cfecf4cce291857e571e30d06877603182b9
Yuhojae <ho_0214@naver.com>
2023-09-06 10:32:06 +0900

    feat : #5 asset 관련 controller 생성

--- backend/src/main/java/com/ssafy/donworry/domain/account/repository/datajpa/BankRepository.java
+++ backend/src/main/java/com/ssafy/donworry/domain/account/repository/datajpa/BankRepository.java
@@ -1,8 +1,9 @@
 package com.ssafy.donworry.domain.account.repository.datajpa;
 
 import com.ssafy.donworry.domain.account.entity.Bank;
+import com.ssafy.donworry.domain.member.entity.FriendRelationship;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BankRepository  {
+public interface BankRepository  extends JpaRepository<Bank, Long>{
 
 }
--- backend/src/main/java/com/ssafy/donworry/domain/account/repository/datajpa/CardCompanyRepository.java
+++ backend/src/main/java/com/ssafy/donworry/domain/account/repository/datajpa/CardCompanyRepository.java
@@ -1,8 +1,9 @@
 package com.ssafy.donworry.domain.account.repository.datajpa;
 
 import com.ssafy.donworry.domain.account.entity.CardCompany;
+import com.ssafy.donworry.domain.member.entity.FriendRelationship;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CardCompanyRepository  {
+public interface CardCompanyRepository  extends JpaRepository<CardCompany, Long>{
 
 }
