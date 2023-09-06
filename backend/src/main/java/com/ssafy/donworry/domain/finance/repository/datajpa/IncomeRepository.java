2aa9af26e461bcbf8efbb525c95ed35c1483ac5c
qhfhdls12 <qhfhdls12@naver.com>
2023-09-06 10:48:50 +0900

    feat: Reop 상속 추가

--- backend/src/main/java/com/ssafy/donworry/domain/finance/repository/datajpa/IncomeRepository.java
+++ backend/src/main/java/com/ssafy/donworry/domain/finance/repository/datajpa/IncomeRepository.java
@@ -1,8 +1,9 @@
 package com.ssafy.donworry.domain.finance.repository.datajpa;
 
+import com.ssafy.donworry.domain.finance.entity.ConsumptionCategory;
 import com.ssafy.donworry.domain.finance.entity.Income;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface IncomeRepository  {
+public interface IncomeRepository extends JpaRepository<Income, Long>{
 
 }
