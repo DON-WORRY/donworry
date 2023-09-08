c3276e521d3122e12960b07e950c16237ee31cf0
qhfhdls12 <qhfhdls12@naver.com>
2023-09-08 16:39:09 +0900

    feat: #39 dutchpay controller 수정

--- backend/src/main/java/com/ssafy/donworry/api/controller/finance/DutchPayController.java
+++ backend/src/main/java/com/ssafy/donworry/api/controller/finance/DutchPayController.java
@@ -1,6 +1,7 @@
 package com.ssafy.donworry.api.controller.finance;
 
 import com.ssafy.donworry.api.controller.finance.requestdto.DutchPayCreateRequest;
+import com.ssafy.donworry.common.api.ApiResult;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,9 +18,9 @@ public class DutchPayController {
 
     @Operation(summary = "더치페이 요청", description = "해당 거래내역에서 더치페이를 요청할 수 있는 API입니다.")
     @PostMapping("/create")
-    public Long createDutchPay(@RequestBody DutchPayCreateRequest dutchPayCreateRequest) {
-        
-        return 3l;
+    public ApiResult<Long> createDutchPay(@RequestBody DutchPayCreateRequest dutchPayCreateRequest) {
+
+        return null;
     }
 
 }
