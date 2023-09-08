779bcef0070fecdc0cfd3466dc29fa1c54ac7de6
LEETAESEONG <ga1754@naver.com>
2023-09-08 17:41:11 +0900

    feat: 회원가입 상태관리

--- frontend/src/components/signups/SignupMiddleInput.tsx
+++ frontend/src/components/signups/SignupMiddleInput.tsx
@@ -1,29 +1,71 @@
 import React from 'react';
-import { View, Text, TextInput, Dimensions, StyleSheet } from 'react-native';
-const SignupMiddleInput: React.FC = () => {
-  return <View>
-    <View>
+import {
+  View,
+  Text,
+  TextInput,
+  Dimensions,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 
-    </View>
-    <TextInput style={[styles.input, styles.inputFirst]} placeholder="    이메일"/>
-    <TextInput style={[styles.input, styles.inputSecond]} placeholder="    인증번호"/>
-    <TextInput style={[styles.input, styles.inputThird]} placeholder="    비밀번호"/>
-    <TextInput style={[styles.input, styles.inputFourth]} placeholder="    비밀번호 재확인"/>
-  </View>;
+interface SignupMiddleInputProps {
+  setEmail: (email: string) => void;
+  setCertificationNumber: (certification: string) => void;
+  setPassword: (password: string) => void;
+  setCheckPassword: (checkPassword: string) => void;
+}
+
+const SignupMiddleInput: React.FC<SignupMiddleInputProps> = (props) => {
+  return (
+    <KeyboardAvoidingView>
+      <TextInput
+        style={[styles.input, styles.inputFirst]}
+        placeholder="이메일"
+        onChangeText={(text) => {
+          props.setEmail(text);
+        }}
+      />
+      <TextInput
+        style={[styles.input, styles.inputSecond]}
+        placeholder="인증번호"
+        onChangeText={(text) => {
+          props.setCertificationNumber(text);
+        }}
+      />
+      <TextInput
+        style={[styles.input, styles.inputThird]}
+        placeholder="비밀번호"
+        onChangeText={(text) => {
+          props.setPassword(text);
+        }}
+      />
+      <TextInput
+        style={[styles.input, styles.inputFourth]}
+        placeholder="비밀번호 재확인"
+        onChangeText={(text) => {
+          props.setCheckPassword(text);
+        }}
+      />
+    </KeyboardAvoidingView>
+  );
 };
-const screenWidth = Dimensions.get("screen").width
+const screenWidth = Dimensions.get('screen').width;
 const styles = StyleSheet.create({
   input: {
     width: screenWidth * 0.9,
     height: 50,
-    borderColor: "gray",
+    borderColor: 'gray',
     borderLeftWidth: 1,
     borderRightWidth: 1,
+    paddingRight: 20,
+    paddingLeft: 20,
   },
   inputFirst: {
     borderTopWidth: 1,
     borderTopLeftRadius: 10,
     borderTopRightRadius: 10,
+    marginTop: 10,
   },
   inputSecond: {
     borderTopWidth: 1,
@@ -36,6 +78,7 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     borderBottomLeftRadius: 10,
     borderBottomRightRadius: 10,
-  }
-})
+    marginBottom: 20,
+  },
+});
 export default SignupMiddleInput;
--- frontend/src/screens/SignupScreen.tsx
+++ frontend/src/screens/SignupScreen.tsx
@@ -1,5 +1,12 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import React, { useState } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
 
 import SignupHeader from '../components/signups/SignupHeader';
 import SignupMiddleInput from '../components/signups/SignupMiddleInput';
@@ -8,16 +15,48 @@ import SignupPrivacyAgreement from '../components/signups/SignupPrivacyAgreement
 import SignupBtn from '../components/signups/SignupBtn';
 
 const SignupScreen: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [certificationNumber, setCertificationNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [checkPassword, setCheckPassword] = useState('');
+
+  const [userName, setUserName] = useState('');
+  const [userGender, setUserGender] = useState('');
+  const [userBirth, setUserBirth] = useState('');
+
+  function signupOper() {
+    console.log([
+      email,
+      certificationNumber,
+      password,
+      checkPassword,
+      userName,
+      userGender,
+      userBirth,
+    ]);
+  }
   return (
-    <View style={styles.container}>
+    <KeyboardAvoidingView style={styles.container}>
       <SignupHeader />
-      <SignupMiddleInput />
-      <SignupBottomInput />
+      <SignupMiddleInput
+        setEmail={setEmail}
+        setCertificationNumber={setCertificationNumber}
+        setPassword={setPassword}
+        setCheckPassword={setCheckPassword}
+      />
+      <SignupBottomInput
+        setUserName={setUserName}
+        setUserGender={setUserGender}
+        setUserBirth={setUserBirth}
+      />
       <SignupPrivacyAgreement />
-      <SignupBtn />
-    </View>
+      <SignupBtn signupOper={signupOper} />
+    </KeyboardAvoidingView>
   );
 };
+
+const screenHeight = Dimensions.get('screen').height;
+const screenWidth = Dimensions.get('screen').width;
 
 const styles = StyleSheet.create({
   container: {
@@ -25,6 +64,8 @@ const styles = StyleSheet.create({
     padding: 20,
     paddingTop: 0,
     backgroundColor: 'white',
+    height: screenHeight,
+    width: screenWidth,
   },
 });
 
