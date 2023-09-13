34813a1a396349a972df1e43f287917ea16294fb
qhfhdls12 <qhfhdls12@naver.com>
2023-09-13 10:17:45 +0900

    feat: #66 소비내역 API 샘플 코드 작성

--- backend/src/main/java/com/ssafy/donworry/api/controller/finance/dto/response/CategoryTotalResponse.java
+++ backend/src/main/java/com/ssafy/donworry/api/controller/finance/dto/response/CategoryTotalResponse.java
@@ -1,14 +1,10 @@
 package com.ssafy.donworry.api.controller.finance.dto.response;
 
 import jakarta.validation.constraints.NotNull;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import static lombok.AccessLevel.PROTECTED;
 
 public record CategoryTotalResponse(
         @NotNull Long total,
-        @NotNull CategoryTotalResponse categoryTotalResponse
+        @NotNull CategoryAmountResponse categoryAmountResponse
 ) {
 
 }
--- backend/src/main/java/com/ssafy/donworry/api/service/finance/request/FinanceCreateServiceRequest.java
+++ backend/src/main/java/com/ssafy/donworry/api/service/finance/request/FinanceCreateServiceRequest.java
@@ -1,14 +1,6 @@
 package com.ssafy.donworry.api.service.finance.request;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-import static lombok.AccessLevel.PROTECTED;
-
-@Getter
-@NoArgsConstructor
-public class FinanceCreateServiceRequest {
+public record FinanceCreateServiceRequest() {
 
 }
 
