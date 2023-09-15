306c02e5417c3e8442bb8fdb1918e972597a4954
Yuhojae <ho_0214@naver.com>
2023-09-15 10:54:23 +0900

    feat : #84 sample코드 작성

---
+++ backend/src/main/java/com/ssafy/donworry/api/controller/account/CardController.java
@@ -0,0 +1,45 @@
+package com.ssafy.donworry.api.controller.account;
+
+import com.ssafy.donworry.api.controller.account.dto.response.CardListResponse;
+import com.ssafy.donworry.api.controller.account.dto.response.ConsumptionResponse;
+import com.ssafy.donworry.api.controller.account.dto.response.ConsumtionDetailResponse;
+import com.ssafy.donworry.common.response.ApiData;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/api/card")
+@RequiredArgsConstructor
+public class CardController {
+
+
+    @Operation(summary = "카드별 소비내역", description = "소지한 카드들의 카드별 소비내역을 조회하는 API입니다.")
+    @GetMapping("/{id}")
+    public ApiData<ConsumptionResponse> searchCardConsumption(@PathVariable("id") Long memberId) {
+        log.info("searchCardConsumption - memberId : " + memberId);
+        ConsumptionResponse list = null;
+        return ApiData.of(list);
+    }
+    @Operation(summary = "상세카드 소비내역", description = "선택한 카드의 사용내역을 조회하는 API입니다.")
+    @GetMapping("/detail/{id}")
+    public ApiData<List<ConsumtionDetailResponse>> searchCardDetailConsumption(@PathVariable("id") Long cardId) {
+        List<ConsumtionDetailResponse> list = null;
+        return ApiData.of(list);
+    }
+
+    @Operation(summary = "사용자 카드 불러오기", description = "사용자가 보유한 카드를 조회하는 API입니다.")
+    @GetMapping("/list/{id}")
+    public ApiData<List<CardListResponse>> searchCardList(@PathVariable("id") Long memberId) {
+        log.info("searchCardList - memberId : " + memberId);
+        List<CardListResponse> list = null;
+        return ApiData.of(list);
+    }
+}
--- backend/src/main/java/com/ssafy/donworry/api/service/account/query/AccountQueryService.java
+++ backend/src/main/java/com/ssafy/donworry/api/service/account/query/AccountQueryService.java
@@ -1,10 +1,17 @@
 package com.ssafy.donworry.api.service.account.query;
 
+import com.ssafy.donworry.api.controller.account.dto.response.AccountAllResponse;
+import com.ssafy.donworry.api.controller.account.dto.response.AccountSummaryResponse;
+import com.ssafy.donworry.domain.account.entity.Account;
 import com.ssafy.donworry.domain.account.repository.query.AccountQueryRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
@@ -12,4 +19,23 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AccountQueryService {
     private final AccountQueryRepository accountQueryRepository;
+
+    public AccountAllResponse searchAccountList(Long memberId) {
+        List<Account> list = accountQueryRepository.findByMemberId(memberId);
+
+        AtomicLong total = new AtomicLong(0L);
+
+
+        List<AccountSummaryResponse> accountSummaryResponses  = list.stream()
+                .map(
+                        account -> {
+                            total.addAndGet(account.getAccountAmount());
+                            return AccountSummaryResponse.of(account);
+                        })
+                .collect(Collectors.toList());
+
+        return AccountAllResponse.of(total.get(), accountSummaryResponses);
+    }
+
+
 }
