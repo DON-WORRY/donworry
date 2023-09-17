4f6789abd1cb67fdab430e280b478a091091d4f2
이태성 <ga1754@naver.com>
2023-09-18 02:32:54 +0900

    feat: chart 만들기

--- frontend/src/components/comparisons/ComparisonChart.tsx
+++ frontend/src/components/comparisons/ComparisonChart.tsx
@@ -1,37 +1,111 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import Svg, { Polygon } from 'react-native-svg';
+import { View } from 'react-native';
+import {
+  VictoryChart,
+  VictoryLine,
+  VictoryPolarAxis,
+  VictoryTheme,
+  VictoryLabel,
+  VictoryArea,
+} from 'victory-native';
 
-type Amount = {
-  food: number;
-  transport: number;
-  hobby: number;
-  life: number;
-  etc: number;
-  style: number;
-};
+const RadarChartExample: React.FC = () => {
+  const adjustLabelPosition = (angle: number, distance: number) => {
+    // 각도를 라디안으로 변환합니다.
+    const radians = (angle / 180) * Math.PI;
 
-interface ComparisonChartProps {
-  size: string;
-  myAmount: Amount;
-  friendAmount: Amount;
-}
+    // 각도에 따라 x와 y 방향으로 이동할 값을 계산합니다.
+    const dx = 20 * Math.cos(radians);
+    const dy = 20 * Math.sin(radians);
 
-const ComparisonChart: React.FC<ComparisonChartProps> = (props) => {
+    return { dx, dy };
+  };
+
+  const data = [
+    { x: 0, y: 2 },
+    { x: 60, y: 3 },
+    { x: 120, y: 5 },
+    { x: 180, y: 1 },
+    { x: 240, y: 4 },
+    { x: 300, y: 2 },
+  ];
+
+  const data2 = [
+    { x: 0, y: 5 },
+    { x: 60, y: 4 },
+    { x: 120, y: 2 },
+    { x: 180, y: 3 },
+    { x: 240, y: 1 },
+    { x: 300, y: 5 },
+  ];
+
   return (
-    <View>
-      <Svg width="200" height="200" viewBox="0 0 100 100">
-        <Polygon
-          points="50,1 95,25 95,75 50,99 5,75 5,25"
-          fill="none"
-          stroke="purple"
-          strokeWidth="3"
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <VictoryChart
+        polar
+        theme={VictoryTheme.material}
+        domain={{ y: [0, 5] }}
+        startAngle={30}
+        endAngle={390}
+      >
+        <VictoryPolarAxis
+          dependentAxis
+          theme={VictoryTheme.material}
+          style={{
+            axis: { stroke: 'none' },
+            ticks: { stroke: 'black' },
+            tickLabels: { fill: 'transparent' },
+            grid: { stroke: 'black' },
+          }}
         />
-      </Svg>
+
+        <VictoryPolarAxis
+          labelPlacement="vertical"
+          tickValues={[0, 60, 120, 180, 240, 300]}
+          tickFormat={['식비', '교통', '생활', '의류', '유흥', '기타']}
+          style={{
+            axis: { stroke: 'black' },
+            ticks: { stroke: 'black' },
+            tickLabels: { fill: 'black' },
+            grid: { stroke: 'black' },
+          }}
+          tickLabelComponent={
+            <VictoryLabel
+              style={[{ fontSize: 25 }]}
+              dx={(d) => adjustLabelPosition(d.angle, 20).dx}
+              dy={(d) => adjustLabelPosition(d.angle, 20).dy}
+            />
+          }
+        />
+
+        <VictoryArea
+          data={data}
+          style={{
+            data: { fill: 'rgba(0, 0, 255, 0.5)' },
+          }}
+        />
+        <VictoryLine
+          data={data}
+          style={{
+            data: { stroke: 'blue' },
+          }}
+        />
+
+        <VictoryArea
+          data={data2}
+          style={{
+            data: { fill: 'rgba(255, 0, 0, 0.5)' },
+          }}
+        />
+        <VictoryLine
+          data={data2}
+          style={{
+            data: { stroke: 'red' },
+          }}
+        />
+      </VictoryChart>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
-
-export default ComparisonChart;
+export default RadarChartExample;
--- frontend/src/screens/bottomTabs/ComparsionScreen.tsx
+++ frontend/src/screens/bottomTabs/ComparsionScreen.tsx
@@ -1,25 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import Svg, { Polygon } from 'react-native-svg';
-
+import ComparisonChart from '../../components/comparisons/ComparisonChart';
 function ComparisonScreen() {
   return (
     <View style={styles.container}>
       <Text>Comparison Screen</Text>
-      <Svg width="200" height="200" viewBox="0 0 100 100">
-        <Polygon
-          points="50,3 97,25 95,75 50,99 3,75 3,25"
-          fill="none"
-          stroke="blue"
-          strokeWidth="2"
-        />
-        <Polygon
-          points="25,50 3,97 25,95 75,50 99,3 75,6"
-          fill="none"
-          stroke="black"
-          strokeWidth="2"
-        />
-      </Svg>
+      <ComparisonChart />
     </View>
   );
 }
@@ -29,6 +15,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'white',
   },
 });
 
