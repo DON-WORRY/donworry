8c236e2131e6329c46843fc2511d393cff33eca0
유승민 <byuri1356@gmail.com>
2023-09-18 12:09:51 +0900

    fix: #210 swagger url mapping fix

--- backend/src/main/java/com/ssafy/donworry/DonworryApplication.java
+++ backend/src/main/java/com/ssafy/donworry/DonworryApplication.java
@@ -1,5 +1,7 @@
 package com.ssafy.donworry;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +11,10 @@ import java.util.TimeZone;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "https://j9c210.p.ssafy.io", description = "Donworry Server URL"),
+							  @Server(url = "http://localhost:8001", description = "Donworry Local URL")
+})
+
 public class DonworryApplication {
 
 	@PostConstruct
