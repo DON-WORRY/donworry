6ec348544393d3ab1c1b98f1caeddd16e3113763
김동현 <qhfhdls12@naver.com>
2023-09-20 03:40:39 +0900

    feat: 카테고리별 소비합계 조회 구현

--- backend/src/main/java/com/ssafy/donworry/api/service/finance/query/FinanceQueryService.java
+++ backend/src/main/java/com/ssafy/donworry/api/service/finance/query/FinanceQueryService.java
@@ -13,9 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Slf4j
 @Service
@@ -29,31 +27,42 @@ public class FinanceQueryService {
     public CategoryTotalResponse searchCategoryTotal(Long memberId) {
         // 1. 소비 데이터 가져오기
         List<Tuple> consumptionList = consumptionQueryRepository.findConsumptionByMemberId(memberId);
+//        for(Tuple t: consumptionList) {
+//            log.info("카테고리 : " + t.get(0, Long.class));
+//            log.info("합계 : " + t.get(1, Long.class));
+//        }
 
-        for(Tuple t: consumptionList) {
-            log.info("카테고리 : " + t.get(0, Long.class));
-            log.info("합계 : " + t.get(1, Long.class));
-        }
         // 2. 더치페이 아이디가 있는 소득 카테고리별로 정리 된 데이터 가져오기
         List<Tuple> incomeList = incomeQueryRepository.findIncomeByMemberId(memberId);
         // ["식비", 1000], ["교통", 2000] ...
-        for(Tuple t: incomeList) {
-            log.info("카테고리 : " + t.get(0, Long.class));
-            log.info("합계 : " + t.get(1, Long.class));
-        }
+//        for(Tuple t: incomeList) {
+//            log.info("카테고리 : " + t.get(0, Long.class));
+//            log.info("합계 : " + t.get(1, Long.class));
+//        }
 
         // 3. 카테고리 별로 소비 - 소득 계산
         List<CategoryAmountResponse> categoryAmountList = createCategoryAmountList(consumptionList, incomeList);
+//        for (CategoryAmountResponse categoryAmountResponse : categoryAmountList) {
+//            log.info("Name : {}, Amount : {}", categoryAmountResponse.category(), categoryAmountResponse.amount());
+//        }
+
+        // 4. 소비 내역 정렬
+        Collections.sort(categoryAmountList, (o1, o2) -> Math.toIntExact(o2.amount() - o1.amount()));
         for (CategoryAmountResponse categoryAmountResponse : categoryAmountList) {
             log.info("Name : {}, Amount : {}", categoryAmountResponse.category(), categoryAmountResponse.amount());
         }
 
-        // 4. 소비 내역 정렬
-
         // 5. 총합 계산 후 dto 생성
+        Long total = createTotal(categoryAmountList);
+        return new CategoryTotalResponse(total, categoryAmountList);
+    }
 
-
-        return null;
+    private Long createTotal(List<CategoryAmountResponse> categoryAmountList) {
+        Long total = 0l;
+        for (CategoryAmountResponse categoryAmountResponse : categoryAmountList) {
+            total += categoryAmountResponse.amount();
+        }
+        return total;
     }
 
     private List<CategoryAmountResponse> createCategoryAmountList(List<Tuple> consumptionList, List<Tuple> incomeList) {
