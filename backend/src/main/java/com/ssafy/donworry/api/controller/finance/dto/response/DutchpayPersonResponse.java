bb8212dc7c9c76a6c015c644ddb1c0a2453f77e8
김동현 <qhfhdls12@naver.com>
2023-09-20 01:41:59 +0900

    feat: DutchpayPersonResponse 리턴값에 id 추가

--- backend/src/main/java/com/ssafy/donworry/api/controller/finance/dto/response/DutchpayPersonResponse.java
+++ backend/src/main/java/com/ssafy/donworry/api/controller/finance/dto/response/DutchpayPersonResponse.java
@@ -3,6 +3,7 @@ package com.ssafy.donworry.api.controller.finance.dto.response;
 import com.ssafy.donworry.domain.finance.entity.enums.DutchpayStatus;
 
 public record DutchpayPersonResponse(
+        Long id,
         String name,
         DutchpayStatus dutchpayStatus,
         Long dutchpayReceivedPrice,
