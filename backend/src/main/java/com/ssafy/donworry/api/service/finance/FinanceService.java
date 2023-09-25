fb3b3f883429c0568b0926ce73a2005ca0c50d8d
qhfhdls12 <qhfhdls12@naver.com>
2023-09-25 13:58:49 +0900

    feat: #213 계좌번호를 이용한 송금 구현

---
+++ backend/src/main/java/com/ssafy/donworry/api/service/finance/FinanceService.java
@@ -0,0 +1,92 @@
+package com.ssafy.donworry.api.service.finance;
+
+import com.ssafy.donworry.api.controller.finance.dto.request.TransferAccountRequest;
+import com.ssafy.donworry.common.error.ErrorCode;
+import com.ssafy.donworry.common.error.exception.EntityNotFoundException;
+import com.ssafy.donworry.common.error.exception.InvalidValueException;
+import com.ssafy.donworry.domain.account.entity.Account;
+import com.ssafy.donworry.domain.account.repository.AccountRepository;
+import com.ssafy.donworry.domain.finance.entity.Consumption;
+import com.ssafy.donworry.domain.finance.entity.ConsumptionCategory;
+import com.ssafy.donworry.domain.finance.entity.Income;
+import com.ssafy.donworry.domain.finance.entity.enums.DutchpayStatus;
+import com.ssafy.donworry.domain.finance.repository.ConsumptionCategoryRepository;
+import com.ssafy.donworry.domain.finance.repository.ConsumptionRepository;
+import com.ssafy.donworry.domain.finance.repository.IncomeRepository;
+import com.ssafy.donworry.domain.member.entity.Member;
+import com.ssafy.donworry.domain.member.repository.MemberRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import static com.ssafy.donworry.domain.finance.entity.enums.DutchpayStatus.NOTSTART;
+
+@Slf4j
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class FinanceService {
+
+    private final MemberRepository memberRepository;
+    private final BCryptPasswordEncoder encoder;
+    private final AccountRepository accountRepository;
+    private final ConsumptionCategoryRepository consumptionCategoryRepository;
+    private final IncomeRepository incomeRepository;
+    private final ConsumptionRepository consumptionRepository;
+
+    public Long transferByAccount(Long memberId, TransferAccountRequest transferAccountRequest) {
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(
+                        () -> new EntityNotFoundException(ErrorCode.MEMBER_NOT_FOUND)
+                );
+        if(!encoder.matches(transferAccountRequest.simplePassword(), member.getMemberSimplePassword())) {
+            throw new InvalidValueException(ErrorCode.SIMPLE_PASSWORD_NOT_MATCH);
+        }
+
+        Account senderAccount = accountRepository.findById(transferAccountRequest.accountId())
+                .orElseThrow(
+                        () -> new EntityNotFoundException(ErrorCode.ACCOUNT_NOT_FOUND)
+                );
+        Account receiverAccount = accountRepository.findByAccountNumber(transferAccountRequest.accountNumber());
+        Long price = transferAccountRequest.price();
+        if(senderAccount.getAccountAmount() < price) {
+            throw new InvalidValueException(ErrorCode.ACCOUNT_NO_MONEY);
+        }
+
+        senderAccount.updateSendAmount(price);
+        receiverAccount.updateReceiveAmount(price);
+
+        accountRepository.save(senderAccount);
+        log.info("save senderAccount : {}", senderAccount.getId());
+        accountRepository.save(receiverAccount);
+        log.info("save receiverAccount : {}", receiverAccount.getId());
+
+        Income income = Income.of(
+                member.getMemberName() + "에게 받음",
+                price,
+                receiverAccount.getAccountAmount(),
+                receiverAccount.getMember(),
+                receiverAccount,
+                senderAccount
+        );
+        Consumption consumption = Consumption.of(
+                receiverAccount.getMember().getMemberName() + "에게 이체",
+                price,
+                senderAccount.getAccountAmount(),
+                NOTSTART,
+                member,
+                senderAccount,
+                receiverAccount,
+                consumptionCategoryRepository.findById(6l).get()
+        );
+
+        incomeRepository.save(income);
+        log.info("save income : {}", income.getId());
+        consumptionRepository.save(consumption);
+        log.info("save consumption : {}", consumption.getId());
+
+        return memberId;
+    }
+}
