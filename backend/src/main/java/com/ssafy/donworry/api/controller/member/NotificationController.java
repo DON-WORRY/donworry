091d9fc16fa6d80933e92eb2541562d8057b14cc
qhfhdls12 <qhfhdls12@naver.com>
2023-09-26 09:27:55 +0900

    feat: #213 sse 구독 요청

--- backend/src/main/java/com/ssafy/donworry/api/controller/member/NotificationController.java
+++ backend/src/main/java/com/ssafy/donworry/api/controller/member/NotificationController.java
@@ -1,16 +1,26 @@
 package com.ssafy.donworry.api.controller.member;
 
+import com.ssafy.donworry.api.service.member.NotificationService;
+import com.ssafy.donworry.common.model.UserDetailsModel;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/notifications")
 @RequiredArgsConstructor
 public class NotificationController {
+    private final NotificationService notificationService;
+    @Operation(summary = "sse 구독 요청", description = "sse를 구독 요청하는 API입니다.")
+    @GetMapping(value = "/subscribe", produces = "text/event-stream")
+    public SseEmitter subscribe(@AuthenticationPrincipal UserDetailsModel userDetailsModel) {
+        Long memberId = userDetailsModel.getId();
+        return notificationService.subscribe(memberId);
+    }
 
 
 
