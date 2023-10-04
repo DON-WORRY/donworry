5bba8734436516afbdea21a6509f508965fd6554
LEETAESEONG <ga1754@naver.com>
2023-10-04 17:36:36 +0900

    fix: UI 수정 및 오류 삭제

--- frontend/src/components/friends/FriendSpendKing.tsx
+++ frontend/src/components/friends/FriendSpendKing.tsx
@@ -249,7 +249,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     width: "100%",
-    height: 60,
+    height: 70,
     marginBottom: 10,
   },
   selectMonthText: {
